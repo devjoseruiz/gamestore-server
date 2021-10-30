d89bba99bc82f62990da70ca73232923b1f8ac60
José Ruiz <joseruiz@keemail.me>
2021-10-30 04:21:49 +0200

    Add Sendgrid email support

--- config/plugins.js
+++ config/plugins.js
@@ -10,4 +10,14 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  email: {
+    provider: "sendgrid",
+    providerOptions: {
+      apiKey: env("SENDGRID_API_KEY"),
+    },
+    settings: {
+      defaultFrom: env("SENDGRID_DEFAULT_FROM"),
+      defaultReplyTo: env("SENDGRID_DEFAULT_REPLY_TO"),
+    },
+  },
 });
